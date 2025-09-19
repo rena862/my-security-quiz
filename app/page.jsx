0d8680914078bc68b822f2b298f83817d50c14e2
rena862 <SDT2595WH0009@edu.sca.ac.jp>
2025-09-19 13:49:19 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,13 @@ import QuizCard from "../components/QuizCard";
 
 export default function HomePage() {
   return (
-    <div>
-      <p className="text-lg text-center">ようこそ！セキュリティクイズへ。</p>
+    <div className="p-6">
+      <p className="text-lg text-center mb-4">ようこそ！セキュリティクイズへ。</p>
+      <div className="grid gap-4">
+        {quizData.map((quiz, index) => (
+          <QuizCard key={index} quiz={quiz} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
-
