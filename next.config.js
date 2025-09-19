2b8b10e5f5cbbd68ebab7d8444a0ebc9467cbab6
rena862 <SDT2595WH0009@edu.sca.ac.jp>
2025-09-19 11:48:12 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/my-security-quiz', // ← リポジトリ名
+  output: 'export', // ← これで静的HTMLを出力
   images: {
-    unoptimized: true,
+    unoptimized: true, // GitHub Pagesで画像を使うなら必要
   },
-};
+  basePath: process.env.GITHUB_PAGES ? '/my-security-quiz' : '',
+  assetPrefix: process.env.GITHUB_PAGES ? '/my-security-quiz/' : '',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
