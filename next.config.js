ce0f9a0eabd82f4865b20bee40e88bbd43221dee
rena862 <SDT2595WH0009@edu.sca.ac.jp>
2025-09-19 11:24:42 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/my-security-quiz', // ← リポジトリ名
+  images: {
+    unoptimized: true,
+  },
+};
 
+module.exports = nextConfig;
