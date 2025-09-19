d7572ebadd2e39377a0616d9515fbddef5dc38e7
rena862 <SDT2595WH0009@edu.sca.ac.jp>
2025-09-19 15:16:54 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').nextconfig} */
-const nextconfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextconfig
+module.exports = nextConfig;
