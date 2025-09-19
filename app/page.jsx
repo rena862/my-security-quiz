cf807ae19632483581024cf31268435c7957076f
rena862 <SDT2595WH0009@edu.sca.ac.jp>
2025-09-19 13:18:40 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
 import quizData from "../data/quiz.json";
 import QuizCard from "../components/QuizCard";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="space-y-6">
-      <h2 className="text-2xl font-bold mb-4">クイズに挑戦しよう！</h2>
-      {quizData.map((q, i) => (
-        <QuizCard key={i} {...q} />
-      ))}
+    <div>
+      <p className="text-lg text-center">ようこそ！セキュリティクイズへ。</p>
     </div>
-  );
+  )
 }
+
